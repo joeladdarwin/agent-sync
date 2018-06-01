c6e077fb902b8e6558cf78e8c49cc68e7e0278f8
sindhupalanivel <sindhuthaman6@gmail.com>
2018-06-01 16:59:08 +0530

    sindhucommitsquare

--- src/app/client/squarefeet/squarefeet.component.ts
+++ src/app/client/squarefeet/squarefeet.component.ts
@@ -6,16 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./squarefeet.component.scss']
 })
 export class SquarefeetComponent implements OnInit {
-  product=[
-    
-    {id:0-750,name:'SF'},
-    {id:751-1500,name:'SF'},
-    {id:2251-3000,name:'SF'},
-    {id:3001-3750,name:'SF'},
-    {id:3751-4500,name:'SF'},
-    {id:4501-5250,name:'SF'},
-    {id:5251,name:'SF'},
-  ]
+
 
   constructor() { }
 
