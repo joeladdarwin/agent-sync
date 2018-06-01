ec25044412613ee9dc67274f943e99bb3a8f8924
sindhupalanivel <sindhuthaman6@gmail.com>
2018-06-01 18:44:34 +0530

    sindhucommitfinal01062018

--- src/app/client/products/products.component.ts
+++ src/app/client/products/products.component.ts
@@ -6,21 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  product=[
-    {name:'Photography'},
-    {name:'Video Tour'},
-    {name:'360 Tour'},
-    {name:'Single Property Website'},
-    {name:'Floor Plans (B&W)'},
-    {name:'Floor Plans (Color)'},
-    {name:'Floor Plans (Color + Furniture)'},
-    {name:'Double-Sided Flyer'},
-    {name:'Brochure (8 Page)'},
-    {name:'Brochure (4 Page)'},
-   
-
-  ]
-
+ 
 
   constructor() { }
 
