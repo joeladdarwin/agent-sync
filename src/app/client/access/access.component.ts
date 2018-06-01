67819ad67436c5e1be9bf2a5a2764569f4ecce25
sindhupalanivel <sindhuthaman6@gmail.com>
2018-06-01 18:11:33 +0530

    sindhucommit01062018

--- src/app/client/access/access.component.ts
+++ src/app/client/access/access.component.ts
@@ -6,15 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./access.component.scss']
 })
 export class AccessComponent implements OnInit {
-  product=[
-    {name:'Meet agent on site'},
-    {name:'Lockbox'},
-    {name:'Access Code'},
-    {name:'Other'},
-   
-   
-
-  ]
+ 
 
   constructor() { }
 
