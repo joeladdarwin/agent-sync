0e6e60f166117150c000b8399bfe3cbe656e62a6
sindhupalanivel <sindhuthaman6@gmail.com>
2018-06-20 11:32:17 +0530

    sindhucommit20062018

---
+++ src/app/client/addons/addons.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addons',
+  templateUrl: './addons.component.html',
+  styleUrls: ['./addons.component.scss']
+})
+export class AddonsComponent implements OnInit {
+  title = "New Order > First Unit";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
