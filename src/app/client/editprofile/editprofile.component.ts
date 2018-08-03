79a6ba5eaa3b0c85dcd675e07f29faa1f18ef359
sindhupalanivel <sindhuthaman6@gmail.com>
2018-08-03 18:07:43 +0530

    firebase

--- src/app/client/editprofile/editprofile.component.ts
+++ src/app/client/editprofile/editprofile.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-editprofile',
   templateUrl: './editprofile.component.html',
   styleUrls: ['./editprofile.component.scss']
 })
-export class EditprofileComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+export class EditprofileComponent {
+  selectedFile:File=null;
+ constructor(private http: HttpClient){}
+  onFileSelected(event){
+    this.selectedFile = <File>event.target.files[0];
   }
+  onUpload(){
+    const fd =new FormData();
+    fd.append('image', this.selectedFile,this.selectedFile.name)
+    this.http.post('https://us-central1-agent-sync-sonder.cloudfunctions.net/helloWorld',fd, {
+      reportProgress:true,
+      observe:'events'
+
+    } )
+    .subscribe(event =>{
+      console.log(event);
+    });
+  }
+ 
 
 }
